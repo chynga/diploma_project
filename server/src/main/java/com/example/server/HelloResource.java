eb9f5f3ab3d77d510234b6740bdb0b677c5453dc
Shynggys <chynga2002@gmail.com>
2022-09-29 21:46:14 +0600

    added JWT library

--- server/src/main/java/com/example/server/HelloResource.java
+++ server/src/main/java/com/example/server/HelloResource.java
@@ -1,5 +1,9 @@
 package com.example.server;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,6 +13,15 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            String token = JWT.create()
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+        }
         return "Hello, World!";
     }
 }
