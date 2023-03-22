b429d55da528bed31d391fe6dc933b9d2e84f135
oloika <106586202+oloika@users.noreply.github.com>
2023-03-22 20:27:29 +0600

    make some changes

--- my_app/lib/view/main.page.dart
+++ my_app/lib/view/main.page.dart
@@ -12,11 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return 
     const Scaffold(
-      backgroundColor: Colors.white,
-       body: Center(
-        child: Text('Home'),
-       
-      ),
+     
     );
   }
 }
