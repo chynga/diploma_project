f91288917aeb3a3b2ba8905b8b814eabcdd39c1c
oloika <camomillel@mail.ru>
2023-05-01 17:07:17 +0600

    added ttimer

--- my_app/lib/features/tooth_clean/view/tooth_clean_page.dart
+++ my_app/lib/features/tooth_clean/view/tooth_clean_page.dart
@@ -3,6 +3,7 @@ import 'package:dental_plaza/core/resources/fonts.gen.dart';
 import 'package:dental_plaza/core/resources/resources.dart';
 import 'package:dental_plaza/features/app/widgets/custom/custom_app_bar.dart';
 import 'package:dental_plaza/features/app/widgets/gradient_bg.dart';
+import 'package:dental_plaza/features/tooth_clean/widget/test_widget.dart';
 import 'package:dental_plaza/features/tooth_clean/widget/youtube_video_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -15,6 +16,7 @@ class ToothCleanPage extends StatefulWidget {
 }
 
 class _ToothCleanPageState extends State<ToothCleanPage> {
+  int seconds = 180;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,89 +45,7 @@ class _ToothCleanPageState extends State<ToothCleanPage> {
             const SizedBox(
               height: 127,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 90,
-                  width: 90,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColors.kBlue,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 15),
-                        blurRadius: 25,
-                        color: AppColors.kBlack.withOpacity(0.32),
-                      )
-                    ],
-                  ),
-                  child: Center(
-                    child: SvgPicture.asset(Assets.icons.play.path),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: 55,
-                  width: 55,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColors.kBlue,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 15),
-                        blurRadius: 25,
-                        color: AppColors.kBlack.withOpacity(0.32),
-                      )
-                    ],
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.remove,
-                      size: 24,
-                      color: AppColors.kWhite,
-                    ),
-                  ),
-                ),
-                const Text(
-                  '3:00',
-                  style: TextStyle(
-                    fontFamily: FontFamily.lcd,
-                    fontSize: 64,
-                    color: AppColors.kBlue,
-                  ),
-                ),
-                Container(
-                  height: 55,
-                  width: 55,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColors.kBlue,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 15),
-                        blurRadius: 25,
-                        color: AppColors.kBlack.withOpacity(0.32),
-                      )
-                    ],
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.add,
-                      size: 24,
-                      color: AppColors.kWhite,
-                    ),
-                  ),
-                ),
-              ],
-            )
+            const TimerWidget()
           ],
         ),
       ),
---
+++ my_app/lib/features/tooth_clean/widget/test_widget.dart
@@ -0,0 +1,219 @@
+import 'dart:async';
+
+import 'package:dental_plaza/core/resources/assets.gen.dart';
+import 'package:dental_plaza/core/resources/fonts.gen.dart';
+import 'package:dental_plaza/core/resources/resources.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TimerWidget extends StatefulWidget {
+  const TimerWidget({
+    super.key,
+  });
+
+  @override
+  State<TimerWidget> createState() => _TimerWidgetState();
+}
+
+class _TimerWidgetState extends State<TimerWidget> {
+  Timer? countdownTimer;
+  Duration myDuration = const Duration(hours: 1);
+  int allSeconds = 180;
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    myDuration = Duration(seconds: allSeconds);
+    // startTimer();
+    super.initState();
+  }
+
+  void startTimer() {
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setCountDown();
+    });
+  }
+
+  // Step 4
+  void stopTimer() {
+    setState(() => countdownTimer!.cancel());
+  }
+
+  void setCountDown() {
+    const reduceSecondsBy = 1;
+    setState(() {
+      final seconds = myDuration.inSeconds - reduceSecondsBy;
+      if (seconds < 0) {
+        countdownTimer!.cancel();
+      } else {
+        myDuration = Duration(seconds: seconds);
+        if (myDuration.inSeconds == 0) {
+          isPlaying = false;
+          FlutterRingtonePlayer.play(
+            android: AndroidSounds.ringtone,
+            ios: IosSounds.glass,
+            looping: false, // Android only - API >= 28
+            volume: 0.5, // Android only - API >= 28
+            asAlarm: false, // Android only - all APIs
+          );
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    countdownTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = strDigits(myDuration.inHours.remainder(24));
+    final minutes = strDigits(myDuration.inMinutes.remainder(60));
+    final seconds = strDigits(myDuration.inSeconds.remainder(60));
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: AppColors.kBlue,
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 15),
+                    blurRadius: 25,
+                    color: AppColors.kBlack.withOpacity(0.32),
+                  )
+                ],
+              ),
+              child: Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(100),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: () {
+                    if (isPlaying) {
+                      stopTimer();
+                    } else {
+                      if (myDuration.inSeconds == 0) {
+                        allSeconds = 180;
+                        myDuration = Duration(seconds: allSeconds);
+                      }
+                      startTimer();
+                    }
+                    isPlaying = !isPlaying;
+                    setState(() {});
+                  },
+                  child: Center(
+                    child: !isPlaying
+                        ? SvgPicture.asset(Assets.icons.play.path)
+                        : const Icon(
+                            Icons.stop_outlined,
+                            color: AppColors.kWhite,
+                            size: 55,
+                          ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 55,
+              width: 55,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: AppColors.kBlue,
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 15),
+                    blurRadius: 25,
+                    color: AppColors.kBlack.withOpacity(0.32),
+                  )
+                ],
+              ),
+              child: Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(100),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: () {
+                    if (myDuration.inSeconds > 60) {
+                      myDuration = Duration(seconds: myDuration.inSeconds - 10);
+                      setState(() {});
+                    }
+                  },
+                  child: const Center(
+                    child: Icon(
+                      Icons.remove,
+                      size: 24,
+                      color: AppColors.kWhite,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              '$minutes:$seconds',
+              style: TextStyle(
+                fontFamily: FontFamily.lcd,
+                fontSize: 64,
+                color: myDuration.inSeconds > 10
+                    ? AppColors.kBlue
+                    : AppColors.kRed,
+              ),
+            ),
+            Container(
+              height: 55,
+              width: 55,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: AppColors.kBlue,
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 15),
+                    blurRadius: 25,
+                    color: AppColors.kBlack.withOpacity(0.32),
+                  )
+                ],
+              ),
+              child: Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(100),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: () {
+                    if (myDuration.inSeconds < 1800) {
+                      myDuration = Duration(seconds: myDuration.inSeconds + 10);
+                      setState(() {});
+                    }
+                  },
+                  child: const Center(
+                    child: Icon(
+                      Icons.add,
+                      size: 24,
+                      color: AppColors.kWhite,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
